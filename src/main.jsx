d4256398fb511f32c9b8d84132d1e49073e97a45
colonnamedia <colonnamedia@gmail.com>
2026-08-30 18:42:33 -0400

    Load creator page styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import './creator.css'
+import './creator-pages.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
